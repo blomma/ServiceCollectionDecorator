12aebeaf83d6ad9486b13a8fdbf3633b74fbe896
Mikael Hultgren <blomma@artsoftheinsane.com>
2021-09-10 09:00:39 +0200

    Slight code cleanup and adding of the comments

--- src/ServiceCollectionDecoratorExtensions.cs
+++ src/ServiceCollectionDecoratorExtensions.cs
@@ -16,7 +16,9 @@ namespace Artsoftheinsane.Extensions.ServiceCollectionDecorator
               s => s.ServiceType == typeof(TInterface));
 
             if (wrappedDescriptor == null)
+            {
                 throw new InvalidOperationException($"{typeof(TInterface).Name} is not registered");
+            }
 
             // create the object factory for our decorator type,
             // specifying that we will supply TInterface explicitly
@@ -37,13 +39,21 @@ namespace Artsoftheinsane.Extensions.ServiceCollectionDecorator
 
         private static object CreateInstance(this IServiceProvider services, ServiceDescriptor descriptor)
         {
+            // this covers the scenario when you register a singleton with an instance
+            // ex. service.AddSingleton<IFoo>(new Foo());
             if (descriptor.ImplementationInstance != null)
+            {
                 return descriptor.ImplementationInstance;
+            }
 
+            // this covers the scenario when a factory has been specified at registration
+            // ex. services.AddScoped<IFoo>(s => new Foo());
             if (descriptor.ImplementationFactory != null)
+            {
                 return descriptor.ImplementationFactory(services);
+            }
 
-
+            // Basic sanity check
             if (descriptor.ImplementationType == null)
             {
                 throw new ArgumentNullException($"{nameof(descriptor.ImplementationType)}");
